bba0ebe41800261b6d67a08aada2f77b9baebdd0
Sébastien Rogier <sebrogier@gmail.com>
2015-01-11 16:03:31 +0100

    Branchement de la structure du site en jekyll

---
+++ assets/js/menu.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.toggle-off-canvas').on('click', function() {
+        console.log("sdfsdf")
+        $(".canvas-wrapper").toggleClass('show-menu-off-canvas');
+    });
+});
+
+
